94d07cf846700ed257e0c1c16692562b901acff9
snan-hub <sgnanwani@gmail.com>
2026-03-19 16:19:56 +0800

    Delete rssb-vercel directory

--- rssb-vercel/api/scan-log.js
+++
@@ -1,7 +0,0 @@
-const { supabase } = require("./_db");
-
-module.exports = async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  const { data } = await supabase.from("scan_log").select("*").order("id", { ascending: false }).limit(1).single();
-  res.json(data || null);
-};
